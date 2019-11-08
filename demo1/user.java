c888fd2c94b8b3d2f8cb647bc56f35650393829b
bianyiit <bianyiit@oracle.com>
2019-11-08 11:35:12 +0800

    新增打印语句

--- demo1/user.java
+++ demo1/user.java
@@ -2,5 +2,9 @@ public class user
 {
 	public static void main(String [] args){
 			System.out.println("hello  github");
+
+			System.out.println("hello  github");
+			System.out.println("hello  github");
+			System.out.println("hello  github");
 	}
 }
