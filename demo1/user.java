4cade67374ea35623180f92483d0190b08bf310b
bianyiit <bianyiit@oracle.com>
2019-11-08 11:31:38 +0800

    新增if判断

--- demo1/user.java
+++ demo1/user.java
@@ -2,5 +2,11 @@ public class user
 {
 	public static void main(String [] args){
 			System.out.println("hello  github");
+
+			System.out.println("hello  github");
+
+			if(1/1 == 0){
+				System.out.println("hello  github");
+			}
 	}
 }
